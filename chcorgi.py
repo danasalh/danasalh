b6379c4e328a35622f6e7f4fe17926cbbe4712ad
dana salh <160002536+danasalh@users.noreply.github.com>
2026-03-21 02:56:42 +0200

    Create chcorgi.py

---
+++ chcorgi.py
@@ -0,0 +1,22 @@
+from PIL import Image , ImageEnhance
+
+im= Image.open("Little power robot.gif")
+
+frames =[]
+
+for frme in range (im.n_frames):
+   im.seek(frame)
+   frameimage =im.convert("RGBA")
+
+   data  = frameimage.getdata()
+   newdata=[]
+   for item in data:
+      if item[0] < 50 and item[1] <50 and item[2] < 50 :
+         newdata.append((255,105,180 ,item[3]))
+      else:
+         newdata.append(item)
+  frameimage.putdata(newdata)
+  frame.append(frameimage)
+
+frams[0].save("Little power robot.gif" , save_all=true , append_image=frames[1:] , loop=0 duration=im.info['duration'])
+
